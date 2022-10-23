8e668a4d375c4a817af078a9614b987300572a35
wailingwailord <94283526+wailingwailord@users.noreply.github.com>
2022-10-23 21:06:24 +0800

    Add files via upload

---
+++ Crop.java
@@ -0,0 +1,37 @@
+public class Crop
+{
+    private String cropName;
+    private int age;
+    private int waterBonus;
+    private int fertilizerBonus;
+    private int waterAmt;
+    private int fertilizerAmt;
+    private int cropCost;
+
+    private void updatePlantStage(int age)
+    {
+
+    }
+
+    private int computeWaterBonus(String cropName, int waterAmt)
+    {
+        int n = 0;
+
+        return n;
+    }
+
+    private int computerFertilizerBonus(String cropName, int fertilizerAmt)
+    {
+        int n = 0;
+
+        return n;
+    }
+
+    private int computeHarvestEarnings(String cropName, int waterBonus, int fertilizerBonus)
+    {
+        int n = 0;
+
+        return n;
+    }
+}
+
---
+++ Time.java
@@ -0,0 +1,9 @@
+public class Time
+{
+    int time = 0;
+
+    public int advanceDay()
+    {
+
+    }
+}
