803a6fffb15f70a49e020a7f48e0a65896381f79
GeckoJnlx3 <103810781+GeckoJnlx3@users.noreply.github.com>
2022-11-30 15:19:10 +0800

    Color Palette Enum finished

---
+++ src/MyFarm/Palette.java
@@ -0,0 +1,22 @@
+package MyFarm;
+
+import java.awt.Color;
+
+public enum Palette {
+    WHITE(0xFFFFFF), 
+    GRASS(0xC0E5C8),
+    UNWATERED_PLOT(0x9F8C83),
+    ROCK(0xBABABA),
+    WATERED_PLOT(0x654f2b),
+    BOTTOM_PANEL(0x5D5D5D);    
+
+    private Color color;
+
+    private Palette(int color){
+        this.color = new Color(color);
+    }
+
+    public Color getColor(){
+        return this.color;
+    }
+}
